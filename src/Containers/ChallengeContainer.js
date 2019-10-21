901cb90cd5f12a0e8b5ae9fa562d50cbc8b947fb
wukrit <sukritwalia@gmail.com>
2019-10-21 15:52:02 -0400

    challenge rendered, form controlled
    Co-authored-by: Shane Lonergan <sptlonergan@gmail.com>

--- src/Containers/ChallengeContainer.js
+++ src/Containers/ChallengeContainer.js
@@ -1,10 +1,41 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 
 const ChallengeContainer = ({challenge}) => {
+    const [wordArr, setWordArr] = useState([])
+    const [totalInput, setTotalInput] = useState([])
+    const [input, setInput] = useState("")
+
+    const handleInput = (event) => {
+        setInput(event.target.value)
+    }
+
+    const compareLetter = (event) => {
+        console.log(input)
+        console.log(event.key)
+        if (event.key === " ") {
+            event.preventDefault()
+            setInput("")
+            setTotalInput([...totalInput, input])
+            return false 
+        }
+    }
 
     return (
         <div className="body-container nes-container is-rounded">
-            {challenge !== {} ? challenge.id : "...loading"}
+            <div className="challenge">
+                {/* <p>{challenge.prompt !== undefined ? challenge.prompt.text : "...loading"}</p> */}
+                <p>{challenge.prompt !== undefined ? challenge.prompt.text : "...loading"} </p>
+                <br />
+                <form className="challenge-input">
+                    <input 
+                        className="nes-input challenge-input" 
+                        type="text" 
+                        value={input}
+                        onChange={handleInput } 
+                        onKeyPress={compareLetter}
+                    />
+                </form>
+            </div>
         </div>
     );
 }

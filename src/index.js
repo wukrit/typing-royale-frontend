3306d413f762a468a0189fe3a876b7a32ababaad
wukrit <sukritwalia@gmail.com>
2019-10-23 18:31:14 -0400

    added import to index
    Co-authored-by: Shane Lonergan <sptlonergan@gmail.com>

--- src/index.js
+++ src/index.js
@@ -5,9 +5,9 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { ActionCableProvider } from 'react-actioncable-provider';
-import API from 'Config/API'
+import API from './Config/API'
 
-console.log(API)
+
 ReactDOM.render(
     <ActionCableProvider url={`${API}/cable`}>
         <Router>
